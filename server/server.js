656d71c4994b9ee1db443ee4a1d411f854ba405a
ed leach <eleach2@yahoo.com>
2017-12-07 17:00:59 -0600

    Add tests for GET todos

--- server/server.js
+++ server/server.js
@@ -22,9 +22,19 @@ app.post('/todos', (req, res) => {
 		});
 });
 
+app.get('/todos', (req,res) => {
+	Todo.find().then((todos) => {
+		res.send({ todos } );
+		}, (e) =>  {
+			res.status(400).send(e);
+		});
+	});
+
 app.listen(3000, () => {
 	console.log("server started on port 3000");
 });
+
+module.exports = { app };
 
 
 // var newTodo = new Todo({
