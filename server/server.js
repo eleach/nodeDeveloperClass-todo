7fb87e29b9ad6c7e5ea7c32ec8f3a32f85d18053
ed leach <eleach2@yahoo.com>
2017-12-15 16:57:01 -0600

    final works

--- server/server.js
+++ server/server.js
@@ -1,21 +1,9 @@
-var env = process.env.NODE_ENV || 'development';
+require('./config/config');
 
-console.log('env = :: ', env   );
-
-if ( env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp'
-
-	} else if ( env === 'test' ) {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest'
-	}
-
-
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -40,8 +28,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    // res.send({todos});
-    res.status(200).send("hello World");
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
@@ -77,62 +64,45 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  // creates body object
+  // takes only two updatable properties from the request body
+  var body = _.pick(req.body, ['text', 'completed']);
 
-///////// utils by Ed
-// remove
-app.get('/remove', (req, res) => {
- 
-  Todo.findByIdAndRemove('5a3284128e1e082f6e414fab').then((t) => {
-    if (!t) {
-      return res.status(404).send("could not find document");
-    }   
- 
-    res.send(t);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
+	// todo completed key was sent in as true
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+	// {$set: body} will set the text and completed values from body object
+	// from body captured above
+	// {new: true} returns the new object
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  }); 
-}); 
-
-// add
-app.get('/add', (req, res) => {
-
-	var myDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-	var myText = "Test todo: " + myDate;
-
-  var todo = new Todo({
-    text:  myText
-  });
-
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-
-})
-
-///////// end utils by Ed
-
-
-
-app.get('/list', (req, res) => {
- 
-  Todo.find().then((c) => {
-    res.send({c});
-    // res.status(200).send("hello World");
-  }, (e) => {
-    res.status(400).send(e);
-  }); 
-}); 
-
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
